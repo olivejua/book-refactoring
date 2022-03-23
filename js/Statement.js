d3b3b4fb6c4733491386f392c9b77d9083fd4b44
olivejua <olivejua@aptner.com>
2022-03-23 21:30:16 +0900

    함수 추출

--- js/Statement.js
+++ js/Statement.js
@@ -1,3 +1,27 @@
+
+function amountFor(perf, play) { //값이 바뀌지 않는 변수는 매개변수로 전달
+    let thisAmount = 0; // 변수를 초기화하는 코드들
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 1000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`알 수 없는 장르: ${play.type}`)
+    }
+
+    return thisAmount;
+}
+
 function statement(invoices, plays) {
     console.log('statement in')
     console.log(invoices);
@@ -9,25 +33,7 @@ function statement(invoices, plays) {
 
     for (let perf of invoices.performances) {
         const play = plays[perf.playID]
-        let thisAmount = 0;
-
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 1000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${play.type}`)
-        }
+        let thisAmount = amountFor(perf, play);
 
         // 포인트 적립
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -43,6 +49,7 @@ function statement(invoices, plays) {
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
 }
+
 
 
 function run() {
---
+++ js_test/test.js
@@ -0,0 +1,3 @@
+
+const invoices = require('invoices.json');
+console.log(invoices);
