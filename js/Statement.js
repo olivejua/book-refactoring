233aaa1afdef6a2e5625a5370b5e4a3c09d6a1d3
olivejua <45840556+olivejua@users.noreply.github.com>
2022-03-24 22:44:45 +0900

    명확한 표현방법 검토

--- js/Statement.js
+++ js/Statement.js
@@ -1,47 +1,46 @@
 
-function amountFor(perf, play) { //값이 바뀌지 않는 변수는 매개변수로 전달
-    let thisAmount = 0; // 변수를 초기화하는 코드들
+function amountFor(aPerformance, play) { //값이 바뀌지 않는 변수는 매개변수로 전달
+    let result = 0; // 변수를 초기화하는 코드들
     switch (play.type) {
         case "tragedy":
-            thisAmount = 40000
-            if (perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
+            result = 40000
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy":
-            thisAmount = 30000;
-            if (perf.audience > 20) {
-                thisAmount += 1000 + 500 * (perf.audience - 20);
+            result = 30000;
+            if (aPerformance.audience > 20) {
+                result += 1000 + 500 * (aPerformance.audience - 20);
             }
-            thisAmount += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
             throw new Error(`알 수 없는 장르: ${play.type}`)
     }
 
-    return thisAmount;
+    return result;
 }
 
 function statement(invoices, plays) {
-    console.log('statement in')
-    console.log(invoices);
-    console.log(plays);
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID]
+    }
 
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoices.customer}\n`
 
     for (let perf of invoices.performances) {
-        const play = plays[perf.playID]
-        let thisAmount = amountFor(perf, play);
+        let thisAmount = amountFor(perf, playFor(perf));
 
         // 포인트 적립
         volumeCredits += Math.max(perf.audience - 30, 0);
         //희극 관객 5명 마다 추가 포인트 제공
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
         //청구 내역 출력
-        result += `총액: ${format(thisAmount/100)} (${perf.audience}석)\n`;
+        result += `${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
         totalAmount += thisAmount
     }
 
