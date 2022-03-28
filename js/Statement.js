815cd8ce8acab9ad74d80a3778bdbf052619fe21
olivejua <45840556+olivejua@users.noreply.github.com>
2022-03-28 23:33:33 +0900

    중첩함수 분리하기

--- js/Statement.js
+++ js/Statement.js
@@ -1,14 +1,18 @@
-function statement(invoices, plays) {
-    let result = `청구 내역 (고객명: ${invoices.customer}\n`
+function statement(invoice, plays) {
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return renderPlainText(statementData, plays);
 
-    for (let perf of invoices.performances) {
-        result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
+        result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
+        return result;
     }
-
-    result += `총액: ${usd(totalAmount())}\n`
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-    return result;
-
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID]
@@ -16,7 +20,7 @@ function statement(invoices, plays) {
 
     function amountFor(aPerformance) { //값이 바뀌지 않는 변수는 매개변수로 전달
         let result = 0; // 변수를 초기화하는 코드들
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy":
                 result = 40000
                 if (aPerformance.audience > 30) {
@@ -31,7 +35,7 @@ function statement(invoices, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
+                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
         }
 
         return result;
@@ -40,30 +44,43 @@ function statement(invoices, plays) {
     function volumeCreditsFor(aPerformance) {
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
         return result;
     }
+
+    function totalAmount(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.amount;
+        }
+        return result;
+    }
+
+    function totalVolumeCredits(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.volumeCredits;
+        }
+        return result;
+    }
+}
+
+function renderPlainText(data) {
+    let result = `청구 내역 (고객명: ${data.customer}\n`
+
+    for (let perf of data.performances) {
+        result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+
+    result += `총액: ${usd(data.totalAmount)}\n`
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    return result;
+
 
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US",
             {style: "currency", currency: "USD", minimumFractionDigits: 2})
             .format(aNumber/100);
-    }
-
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of invoices.performances) {
-            result += volumeCreditsFor(perf);
-        }
-        return result;
-    }
-
-    function totalAmount() {
-        let result = 0;
-        for (let perf of invoices.performances) {
-            result += amountFor(perf)
-        }
-        return result;
     }
 }
 
