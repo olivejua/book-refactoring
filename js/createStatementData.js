09bc444d7bb436cfd3430408eb93a6d9df67808f
olivejua <45840556+olivejua@users.noreply.github.com>
2022-03-28 23:50:53 +0900

    분리한 함수 별도 파일로 저장

--- js/createStatementData.js
+++ js/createStatementData.js
@@ -1,10 +1,10 @@
-function statement(invoice, plays) {
+function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
     statementData.totalAmount = totalAmount(statementData);
     statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return renderPlainText(statementData, plays);
+    return statementData;
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
@@ -49,11 +49,8 @@ function statement(invoice, plays) {
     }
 
     function totalAmount(data) {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
+        return data.performances
+            .reduce((total, p) => total + p.amount, 0);
     }
 
     function totalVolumeCredits(data) {
@@ -64,37 +61,3 @@ function statement(invoice, plays) {
         return result;
     }
 }
-
-function renderPlainText(data) {
-    let result = `청구 내역 (고객명: ${data.customer}\n`
-
-    for (let perf of data.performances) {
-        result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-    }
-
-    result += `총액: ${usd(data.totalAmount)}\n`
-    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
-    return result;
-
-
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-            {style: "currency", currency: "USD", minimumFractionDigits: 2})
-            .format(aNumber/100);
-    }
-}
-
-
-
-function run() {
-    getJSONValue('/json/invoices.json')
-        .then(invoices => {
-            getJSONValue('/json/plays.json')
-                .then(plays => {
-                    const result = statement(invoices[0], plays);
-                    console.log(result);
-                })
-        })
-}
-
-run();
